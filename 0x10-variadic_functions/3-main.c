574125da11f9d973510d5a97fb803282521b4d17
Jovany21kamel <135173255+Jovany21kamel@users.noreply.github.com>
2023-08-17 16:05:36 +0300

    task

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char main(void)
+{
+    int i = 11;
+    printf("Hello world\n");
+    return(i);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,5 @@
+# Variadic functions in C programming:
+ 1. what are Variadic functions and how to use them
+ 2. solving and checking with files inputs
+ 
+## ALX Tasks and quizes
